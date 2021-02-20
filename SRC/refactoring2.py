35603791c700e2e7b0d6bcd8135ce3b125a4e1c1
Arun Gandhi <75817724+ArunG-dotcom@users.noreply.github.com>
2021-02-20 12:39:04 -0600

    Update refactoring2.py

--- SRC/refactoring2.py
+++ SRC/refactoring2.py
@@ -4,7 +4,9 @@ Created on Thu Jan 14 19:12:43 2021
 
 @author: reach
 """
+#------------------------------------------------------------------------------
 # Nan - not a number
+#importing packages
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
@@ -15,21 +17,20 @@ import pandas as pd
 import numpy as np
 import re
 
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('average_perceptron_tagger')
+nltk.download('maxent_treebank_pos_tagger')
+#------------------------------------------------------------------------------
 def get(filename, bookname):
-    #Setting the path to the first book in the harry potter series.
-
-    nltk.download('stopwords')
-    nltk.download('punkt')
-    nltk.download('average_perceptron_tagger')
-    nltk.download('maxent_treebank_pos_tagger')
-
-    
-    
 
 #opening,reading,translating(replacing),and spliting the sorcerors stone file
     with open(filename,"r",encoding='utf8',errors="ignore") as f:
         text_list = f.read().lower().split()
-    #takes out punctuation lines 32 - 45  
+        
+    #--------------------------------------------------------------------------  
+    #takes out punctuation lines 32 - 45 
+    #stopwords is a list of common words that can be removed
     stop_list = []
     stop_words = list(stopwords.words("english"))
     for word in text_list:
@@ -37,52 +38,69 @@ def get(filename, bookname):
             pass
         else:
             stop_list.append(word) 
-        
+    #--------------------------------------------------------------------------       
+    #translation is a built in function which helps with changing
+    #the characters which I am using to remove punctuation.   
     trans = str.maketrans("","",'~!@#$%^&*()`,.<>/?\\|[]{};-\n\':"') 
     strip_list = []
     for word in stop_list:
         word = word.translate(trans)
         strip_list.append(word) # adding the single item to a existing list
-            
+    #--------------------------------------------------------------------------
+    #stem helps with breaking the word down to its base/root
     stem_list = []
     ss = SnowballStemmer("english")
     for word in strip_list:
         word = ss.stem(word)
         stem_list.append(word)
-    #Returns all possible ngrams generated from a sequence of items,
-    #as an iterator.
-    #converts to a list
+    #--------------------------------------------------------------------------
+    #Returns all possible ngrams generated from the text.
     ngram = list(everygrams(stem_list, min_len = 3, max_len = 5))  
     # adds any item to the exisisting list/dictionary
     count_dict = defaultdict(lambda: 0)
+    # counting the number of ngrams.
     for tuples in ngram:
         count_dict[tuples] += 1
     
-    df = pd.DataFrame(count_dict, index = [bookname])# corresponds with line55
-    
-            
+    df = pd.DataFrame(count_dict, index = [bookname])
             
     return df
-
+#------------------------------------------------------------------------------
 if __name__=="__main__":
+    # getting the sorcerors stone file
     df1 = get('../Data/sorcerors_stone.txt', 'sorcerors_stone') 
+    # getting the fellowship_of_ring
     df2 = get('../Data/fellowship_of_ring.txt', 'fellowship_ring')
+    ## getting the unkown file
     df3 = get('../Data/unknown.txt','unknown')
+    #--------------------------------------------------------------------------
+    # concat combines the text from Df1,Df2,Df3
     df = pd.concat([df1,df2,df3]).fillna(0)
-    print(df)
+    #--------------------------------------------------------------------------
     # distance from orgin - linealg function
+    # this line helps calculate the distance between the unkown and the
+    #sorcerors stone
     d = np.linalg.norm(df.loc['sorcerors_stone'] - df.loc['unknown']) 
-    print("The distance between the sorcerors stone and the unknown")
-    print(d)
+    print("The distance between the sorcerors stone and the unknown", int(d))
 
     # distance from orgin - linealg function
     #The loc property is used to access a group of rows and columns by label(s)
     #or a boolean array. . loc[] is primarily label based, but may also be used
     #with a boolean array.
+    #This line helps calculate the distance between the unkown and the
+    #fellowship_of_ring
     c = np.linalg.norm(df.loc['fellowship_ring'] - df.loc['unknown']) 
-    print("The distance between the fellowship ring and the unknown")
-    print(c)
-    print("The sorcerors stone is similar to the unkwon book")
+    print("The distance between the fellowship ring and the unknown", int(c))
+
+    print("The unknown book is more similar to the sorcerors stone book.")
+    #--------------------------------------------------------------------------
+    
+                      
+
+
+  
+
+   
               
 
     
